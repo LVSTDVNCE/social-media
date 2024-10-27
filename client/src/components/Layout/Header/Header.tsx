7c522a14b1dad267dc7f4d830d6bbcc2d3b66c35
Mikhail <mtarantin534@gmail.com>
2024-10-27 19:27:01 +0400

    styled layout (header & sidebar)

--- client/src/components/Layout/Header/Header.tsx
+++ client/src/components/Layout/Header/Header.tsx
@@ -1,7 +1,71 @@
 import React from 'react'
+import { Layout, Input, Avatar, Badge, Menu, Dropdown } from 'antd'
+import { BellOutlined, UserOutlined } from '@ant-design/icons'
+import { Link } from 'react-router-dom'
+
+const { Header: AntHeader } = Layout
+const { Search } = Input
+
+const headerStyles: React.CSSProperties = {
+	display: 'flex',
+	justifyContent: 'space-between',
+	alignItems: 'center',
+	padding: '0 207px',
+	backgroundColor: '#3b5998',
+	position: 'relative',
+	zIndex: 1,
+}
+
+const logoStyles: React.CSSProperties = {
+	fontSize: 24,
+	color: '#fff',
+	fontWeight: 'bold',
+}
+
+const iconStyles: React.CSSProperties = {
+	fontSize: 20,
+	color: '#fff',
+	marginLeft: 20,
+	cursor: 'pointer',
+}
 
 const Header: React.FC = () => {
-	return <div>header</div>
+	const profileMenu = (
+		<Menu>
+			<Menu.Item key='0'>
+				<Link to='/UserProfile'>Мой профиль</Link>
+			</Menu.Item>
+			<Menu.Item key='1'>
+				<Link to='/'>Настройки</Link>
+			</Menu.Item>
+			<Menu.Divider />
+			<Menu.Item key='2'>Выйти</Menu.Item>
+		</Menu>
+	)
+
+	return (
+		<AntHeader style={headerStyles}>
+			<div style={{ display: 'flex', alignItems: 'center', gap: '20px' }}>
+				<Link to='/' style={logoStyles}>
+					SGU
+				</Link>
+				<Search placeholder='Поиск' allowClear style={{ maxWidth: 400 }} />
+			</div>
+
+			<div style={{ display: 'flex', alignItems: 'center' }}>
+				<Badge>
+					<BellOutlined style={iconStyles} />
+				</Badge>
+
+				<Dropdown overlay={profileMenu} trigger={['click']}>
+					<Avatar
+						style={{ marginLeft: 20, cursor: 'pointer' }}
+						icon={<UserOutlined />}
+					/>
+				</Dropdown>
+			</div>
+		</AntHeader>
+	)
 }
 
 export default Header
--- client/src/components/Layout/SideBar/SideBar.tsx
+++ client/src/components/Layout/SideBar/SideBar.tsx
@@ -1,7 +1,56 @@
+// SideBar.tsx
 import React from 'react'
+import { Menu } from 'antd'
+import {
+	FileAddOutlined,
+	FileSearchOutlined,
+	HomeOutlined,
+	MessageOutlined,
+	UserOutlined,
+} from '@ant-design/icons'
+import { Link, useLocation } from 'react-router-dom'
+
+const { Item } = Menu
+
+// Стили для боковой панели
+const sidebarStyles: React.CSSProperties = {
+	marginLeft: 180,
+	width: 200,
+	marginTop: -64,
+	paddingTop: 64,
+	height: '100vh',
+	backgroundColor: '#f0f2f5',
+	borderRight: '1px solid #d9d9d9',
+	overflow: 'hidden',
+}
 
 const SideBar: React.FC = () => {
-	return <div>nav</div>
+	const location = useLocation()
+
+	return (
+		<Menu
+			mode='inline'
+			selectedKeys={[location.pathname]}
+			style={sidebarStyles}
+			defaultSelectedKeys={['/']}
+		>
+			<Item key='/UserProfile' icon={<UserOutlined />}>
+				<Link to='/UserProfile'>Профиль</Link>
+			</Item>
+			<Item key='/' icon={<HomeOutlined />}>
+				<Link to='/'>Новости</Link>
+			</Item>
+			<Item key='/Messenger' icon={<MessageOutlined />}>
+				<Link to='/Messenger'>Мессенджер</Link>
+			</Item>
+			<Item key='/NewPost' icon={<FileAddOutlined />}>
+				<Link to='/NewPost'>Создать пост</Link>
+			</Item>
+			<Item key='/SearchPage' icon={<FileSearchOutlined />}>
+				<Link to='/SearchPage'>Поиск постов</Link>
+			</Item>
+		</Menu>
+	)
 }
 
 export default SideBar
